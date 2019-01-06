c23ef44ea66a316d3a2bf343d37a646d2551d472
dlexion <dlexion@yandex.ru>
2019-01-06 22:30:06 +0300

    Add project files.

---
+++ SalesStatistics.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesStatistics.Web.Startup))]
+namespace SalesStatistics.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
