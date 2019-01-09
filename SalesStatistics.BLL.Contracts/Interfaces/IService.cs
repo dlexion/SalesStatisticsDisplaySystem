c53ade3ab5eee6e29148c879ff22d8f068d7090e
dlexion <dlexion@yandex.ru>
2019-01-09 17:46:37 +0300

    Add interface for service classes.

---
+++ SalesStatistics.BLL.Contracts/Interfaces/IService.cs
@@ -0,0 +1,44 @@
+﻿using SalesStatistics.DAL.Contracts.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SalesStatistics.BLL.Contracts.Interfaces
+{
+    // TODO move DTO objects to separate assembly
+    // TODO split interface into several simpler interfaces
+    // TODO make generic interface with add, getAll, remove and update methods
+    public interface IService
+    {
+        void AddCustomer(CustomerDTO customer);
+        IEnumerable<CustomerDTO> GetAllCustomers();
+        CustomerDTO GetCustomerById(int id);
+        CustomerDTO GetCustomerByName(string firstName, string lastName);
+        void RemoveCustomer(CustomerDTO customer);
+        void UpdateCustomer(CustomerDTO customer);
+
+        void AddItem(ItemDTO item);
+        IEnumerable<ItemDTO> GetAllItems();
+        ItemDTO GetItemById(int id);
+        ItemDTO GetItemByName(string name);
+        void RemoveItem(ItemDTO item);
+        void UpdateItem(ItemDTO item);
+
+        void AddManager(ManagerDTO manager);
+        IEnumerable<ManagerDTO> GetAllManagers();
+        ManagerDTO GetManagerById(int id);
+        ManagerDTO GetManagerByName(string lastName);
+        void RemoveManager(ManagerDTO manager);
+        void UpdateManager(ManagerDTO manager);
+
+        void AddOrder(OrderDTO order);
+        IEnumerable<OrderDTO> GetAllOrders();
+        IEnumerable<OrderDTO> GetOrdersByDate(DateTime leftBound, DateTime rightBound);
+        IEnumerable<OrderDTO> GetOrdersByManager(int managerId);
+        IEnumerable<OrderDTO> GetOrdersByItem(int itemId);
+        IEnumerable<OrderDTO> GetOrdersByCustomer(int customerId);
+        OrderDTO GetOrderById(int id);
+        void RemoveOrder(OrderDTO order);
+        void UpdateOrder(OrderDTO order);
+    }
+}
