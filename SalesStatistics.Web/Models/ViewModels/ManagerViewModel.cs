c79eb900e8fbf0d4ec24902e20bcb8cfca93f5f9
dlexion <dlexion@yandex.ru>
2019-01-10 09:38:52 +0300

    Create manager view model

---
+++ SalesStatistics.Web/Models/ViewModels/ManagerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesStatistics.Web.Models.ViewModels
+{
+    public class ManagerViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(25, ErrorMessage = "Last Name must contain less than 25 characters")]
+        public string LastName { get; set; }
+    }
+}
