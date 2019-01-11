9e68cadf4dc88c00d2ffadf21d0834b2781e29ab
dlexion <dlexion@yandex.ru>
2019-01-11 18:14:33 +0300

    Remove virtual keyword.

--- SalesStatistics.DAL.Contracts/DTO/OrderDTO.cs
+++ SalesStatistics.DAL.Contracts/DTO/OrderDTO.cs
@@ -6,8 +6,8 @@
         public System.DateTime Date { get; set; }
         public decimal Cost { get; set; }
 
-        public virtual ManagerDTO Manager { get; set; }
-        public virtual CustomerDTO Customer { get; set; }
-        public virtual ItemDTO Item { get; set; }
+        public ManagerDTO Manager { get; set; }
+        public CustomerDTO Customer { get; set; }
+        public ItemDTO Item { get; set; }
     }
 }
