5f61222e9e03bdd1c811d77a3d820bf1b934a5f1
dlexion <dlexion@yandex.ru>
2019-01-13 19:50:59 +0300

    Create roles if they are not exist

--- SalesStatistics.Web/Startup.cs
+++ SalesStatistics.Web/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SalesStatistics.Web.Models;
 
 [assembly: OwinStartupAttribute(typeof(SalesStatistics.Web.Startup))]
 namespace SalesStatistics.Web
@@ -9,6 +12,43 @@ namespace SalesStatistics.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+
+                var userPassword = "Admin_123";
+
+                var createResult = userManager.Create(user, userPassword);
+
+                if (createResult.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
         }
     }
+
 }
