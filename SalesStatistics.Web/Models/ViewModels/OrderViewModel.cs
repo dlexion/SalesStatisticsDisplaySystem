ab27f24c444745d803bccf46013ed1575636cefa
dlexion <dlexion@yandex.ru>
2019-01-14 07:35:53 +0300

    Add order view model.

---
+++ SalesStatistics.Web/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SalesStatistics.DataTransferObjects;
+
+namespace SalesStatistics.Web.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        public System.DateTime Date { get; set; }
+
+        [Required]
+        [Display(Name = "Cost")]
+        public decimal Cost { get; set; }
+
+        public ManagerDTO Manager { get; set; }
+        public CustomerDTO Customer { get; set; }
+        public ItemDTO Item { get; set; }
+    }
+}
