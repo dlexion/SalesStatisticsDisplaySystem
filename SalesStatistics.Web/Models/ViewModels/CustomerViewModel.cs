fe719ffc873d9eb87e6d97559bc137a1136bd41c
dlexion <dlexion@yandex.ru>
2019-01-14 06:34:01 +0300

    Add customer viewv model.

---
+++ SalesStatistics.Web/Models/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesStatistics.Web.Models.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(25, ErrorMessage = "First Name must contain less than 25 characters")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(25, ErrorMessage = "Last Name must contain less than 25 characters")]
+        public string LastName { get; set; }
+    }
+}
