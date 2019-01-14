df9cfaf4a8ad981d2c3cbba2914caac492bfe17d
dlexion <dlexion@yandex.ru>
2019-01-14 06:32:35 +0300

    Change display name.

--- SalesStatistics.Web/Models/ViewModels/ItemViewModel.cs
+++ SalesStatistics.Web/Models/ViewModels/ItemViewModel.cs
@@ -7,7 +7,7 @@ namespace SalesStatistics.Web.Models.ViewModels
         public int Id { get; set; }
 
         [Required]
-        [Display(Name = "Item")]
+        [Display(Name = "Name")]
         [StringLength(25, ErrorMessage = "Name must contain less than 25 characters")]
         public string Name { get; set; }
     }
